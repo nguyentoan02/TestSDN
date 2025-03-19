d00632ef8875849d6bbc0fde87cbc9e8acfb6331
ToanNV <144198300+nguyentoan02@users.noreply.github.com>
2025-03-19 18:44:15 +0700

    test1 (#1)

--- models/user.js
+++ models/user.js
@@ -3,6 +3,10 @@ const mongoose = require('mongoose');
 const UserSchema = new mongoose.Schema({
     username: { type: String, required: true, unique: true },
     password: { type: String, required: true },
+
+    // role: user or admin  
+
+            
     role: { type: String, enum: ['user', 'admin'], default: 'user' }
 });
 
